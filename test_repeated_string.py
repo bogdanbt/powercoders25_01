7e9c549942d1c83a40269182534c89c9554c7e17
bogdanbt <bt.tarasenko@gmail.com>
2025-06-22 15:00:09 +0200

    prepare folder delete old tests

--- test_repeated_string.py
+++
@@ -1,7 +0,0 @@
-from repeated_string import repeated_string
-
-def test_repeated_string():
-    """Test the repeated_string function"""
-    
-    # Test cases
-    assert repeated_string("aba",10) == 7
--- test_sock_merchant.py
+++
@@ -1,6 +0,0 @@
-from sock_merchant import sock_merchant
-
-def test_sock_merchant_basic():
-    """Test the basic functionality of sock_merchant function"""
-    assert sock_merchant([1, 2, 1, 2, 1, 3, 2]) == 2
-    assert sock_merchant([10, 20, 20, 10, 10, 30]) == 2
